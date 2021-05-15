e307bdfd8a8f9fc41f77694fb750de373dd33f64
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 00:08:51 -0300

    chore: criadas funcoes auxiliares para gerar ormconfig

--- src/config/typeorm.module.ts
+++ src/config/typeorm.module.ts
@@ -1,6 +1,25 @@
+import { resolve } from "path";
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 
+export const getTypeOrmModuleOptions = (): TypeOrmModuleOptions => ({
+  type: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.POSTGRES_PORT || 5432),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  entities: [`${resolve(__dirname, "..")}/**/typeorm/*.entity.{ts,js}`],
+  logging: true,
+});
+
+export const getTypeOrmMigrationOptions = (): TypeOrmModuleOptions => ({
+  ...getTypeOrmModuleOptions(),
+  entities: ["dist/**/typeorm/*.entity.js"],
+  migrations: ["database/migrations/*.{ts,js}"],
+  cli: {
+    migrationsDir: "database/migrations",
+  },
+});
 @Module({
   imports: [TypeOrmModule.forRoot()],
 })
