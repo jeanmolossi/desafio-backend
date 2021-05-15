0b163e8714adb046977bec90048fdfee3875aea3
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 02:21:33 -0300

    chore: adicionado modulo de transactions ao modulo da aplicacao

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@/config/config.module";
 import { UserModule } from "@/user/user.module";
 import { AuthModule } from "./auth/auth.module";
+import { TransactionsModule } from "@/transactions/transactions.module";
 
 @Module({
-  imports: [ConfigModule, AuthModule, UserModule],
+  imports: [ConfigModule, AuthModule, UserModule, TransactionsModule],
 })
 export class ApplicationModule {}
