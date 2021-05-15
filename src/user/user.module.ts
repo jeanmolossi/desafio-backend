e9fb9dd2b28c78cf749d11eea2400df2f9da0bbd
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 01:15:12 -0300

    chore: injecao de repositorio resolvida

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,20 @@
-import { Module } from "@nestjs/common";
+import { Module, Provider } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { CreateUserService } from "./services";
+import { TypeOrmUserRepositoryAdapter } from "./typeorm/typeorm-user.repository";
+import { Connection } from "typeorm";
+
+const persistence_provider: Provider[] = [
+  {
+    provide: TypeOrmUserRepositoryAdapter,
+    inject: [Connection],
+    useFactory: (connection: Connection) =>
+      connection.getCustomRepository(TypeOrmUserRepositoryAdapter),
+  },
+];
 
 @Module({
   controllers: [UserController],
-  providers: [CreateUserService],
+  providers: [...persistence_provider, CreateUserService],
 })
 export class UserModule {}
