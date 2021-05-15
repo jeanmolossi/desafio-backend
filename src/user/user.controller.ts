aed94ccaff3823b357629421ef99341dc77edceb
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 01:02:56 -0300

    chore: injetando e utilizando service dentro do controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from "@/application/validation.pipe";
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { CreateUserAdapter } from "./adapters/CreateUserAdapter";
+import { CreateUserService } from "./services";
 
 @Controller("user")
 export class UserController {
+  constructor(
+    @Inject(CreateUserService)
+    private readonly createUserService: CreateUserService
+  ) {}
+
   @Post()
   async createUser(@Body(ValidationPipe) createUserPayload: CreateUserAdapter) {
-    return createUserPayload;
+    const created_user = await this.createUserService.execute(
+      createUserPayload
+    );
+
+    return created_user;
   }
 }
