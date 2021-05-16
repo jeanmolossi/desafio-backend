cd2533c2198b9c4594b2f7978223d88f4768cc4b
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 23:31:51 -0300

    chore: criado o controller de transactions

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { ValidationPipe } from "@/application/validation.pipe";
+import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { CreateTransactionAdapter } from "./adapters/create-transaction-adapter";
+import { CreateTransactionService } from "./services/create/create-transaction.service";
 
-@Controller('transactions')
-export class TransactionsController {}
+@Controller("transactions")
+export class TransactionsController {
+  constructor(
+    @Inject(CreateTransactionService)
+    private readonly createTransactionService: CreateTransactionService
+  ) {}
+
+  @Post()
+  async createTransaction(
+    @Body(ValidationPipe) createTransactionPayload: CreateTransactionAdapter
+  ) {
+    const transaction = await this.createTransactionService.execute(
+      createTransactionPayload
+    );
+
+    return transaction;
+  }
+}
