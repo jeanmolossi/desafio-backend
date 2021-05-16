a375a8f6cf4579cf0beef71825d84ae1bc9a065c
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-16 01:12:53 -0300

    chore: adicionado endpoint de busca

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -3,15 +3,19 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   Inject,
   Param,
   Post,
   Put,
+  Query,
 } from "@nestjs/common";
 import { CreateTransactionAdapter } from "./adapters/create-transaction-adapter";
+import { FilterTransactionAdapter } from "./adapters/filter-transaction-adapter";
 import { UpdateTransactionAdapter } from "./adapters/update-transaction-adapter";
 import { CreateTransactionService } from "./services/create/create-transaction.service";
 import { DeleteTransactionService } from "./services/delete-transaction/delete-transaction.service";
+import { SearchTransactionService } from "./services/search-transaction/search-transaction.service";
 import { UpdateTransactionService } from "./services/update-transaction/update-transaction.service";
 
 @Controller("transactions")
@@ -24,8 +28,18 @@ export class TransactionsController {
     private readonly updateTransactionService: UpdateTransactionService,
 
     @Inject(DeleteTransactionService)
-    private readonly deleteTransactionService: DeleteTransactionService
+    private readonly deleteTransactionService: DeleteTransactionService,
+
+    @Inject(SearchTransactionService)
+    private readonly searchTransactionService: SearchTransactionService
   ) {}
+
+  @Get()
+  async searchTransaction(
+    @Query(ValidationPipe) filters: FilterTransactionAdapter
+  ) {
+    return this.searchTransactionService.execute(filters);
+  }
 
   @Post()
   async createTransaction(
