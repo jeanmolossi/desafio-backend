28ea3a282c6ab391e144a46f04ab9c975a21c53d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-16 00:42:07 -0300

    chore: adicionado endpoint para atualizar a transaction

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,13 +1,18 @@
 import { ValidationPipe } from "@/application/validation.pipe";
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Param, Post, Put } from "@nestjs/common";
 import { CreateTransactionAdapter } from "./adapters/create-transaction-adapter";
+import { UpdateTransactionAdapter } from "./adapters/update-transaction-adapter";
 import { CreateTransactionService } from "./services/create/create-transaction.service";
+import { UpdateTransactionService } from "./services/update-transaction/update-transaction.service";
 
 @Controller("transactions")
 export class TransactionsController {
   constructor(
     @Inject(CreateTransactionService)
-    private readonly createTransactionService: CreateTransactionService
+    private readonly createTransactionService: CreateTransactionService,
+
+    @Inject(UpdateTransactionService)
+    private readonly updateTransactionService: UpdateTransactionService
   ) {}
 
   @Post()
@@ -17,6 +22,19 @@ export class TransactionsController {
     const transaction = await this.createTransactionService.execute(
       createTransactionPayload
     );
+
+    return transaction;
+  }
+
+  @Put("/:id")
+  async updateTransaction(
+    @Param("id") transaction_id: string,
+    @Body(ValidationPipe) updateTransactionPayload: UpdateTransactionAdapter
+  ) {
+    const transaction = await this.updateTransactionService.execute({
+      ...updateTransactionPayload,
+      id: transaction_id,
+    });
 
     return transaction;
   }
