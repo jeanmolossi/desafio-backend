9a19a2d370cfb14b238081990bce0f007fbd62d9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 23:18:30 -0300

    chore: criado service para criar transactions

--- src/transactions/services/create/create.service.ts
+++ src/transactions/services/create/create.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { CreateTransactionAdapter } from "@/transactions/adapters/create-transaction-adapter";
+import { Transactions } from "@/transactions/transactions.entity";
+import { TransactionsRepository } from "@/transactions/types/transactions.repository";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class CreateService {}
+export class CreateService {
+  constructor(
+    private readonly transactionsRepository: TransactionsRepository
+  ) {}
+
+  async execute(payload: CreateTransactionAdapter) {
+    const transaction = new Transactions(payload);
+
+    const newTransaction = await this.transactionsRepository.createTransaction(
+      transaction
+    );
+
+    return newTransaction;
+  }
+}
