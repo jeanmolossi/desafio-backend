d2c9ab75bd93c64379ca5bac2d323290dafc9993
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-15 23:31:26 -0300

    chore: injetado o adapter de repositorio no service

--- src/transactions/services/create/create-transaction.service.ts
+++ src/transactions/services/create/create-transaction.service.ts
@@ -1,11 +1,14 @@
 import { CreateTransactionAdapter } from "@/transactions/adapters/create-transaction-adapter";
 import { Transactions } from "@/transactions/transactions.entity";
+import { TypeOrmTransactionsRepositoryAdapter } from "@/transactions/typeorm/typeorm-transactions.repository";
 import { TransactionsRepository } from "@/transactions/types/transactions.repository";
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class CreateTransactionService {
   constructor(
+    @InjectRepository(TypeOrmTransactionsRepositoryAdapter)
     private readonly transactionsRepository: TransactionsRepository
   ) {}
 
